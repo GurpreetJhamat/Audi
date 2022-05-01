3f63543fb1ecdddd3792326850f1a7c7d2c3e16d
GurpreetJhamat <gjhammt@gmail.com>
2022-05-01 01:52:45 -0400

    added new sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,21 @@
-const nav = document.querySelector('.nav')
-const menuToggle = document.querySelector('.toggle');
-const navHidden = document.querySelector('.nav__hidden')
+const nav = document.querySelector(".nav");
+const menuToggle = document.querySelector(".toggle");
+const navHidden = document.querySelector(".nav__hidden");
 
-menuToggle.addEventListener('click', () => {
-    navHidden.classList.toggle('expand')
-    nav.classList.toggle('open')
-})
+menuToggle.addEventListener("click", () => {
+  navHidden.classList.toggle("expand");
+  nav.classList.toggle("open");
+});
+
+const slider = document.querySelector(".showcase__images");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.addEventListener("click", () => {
+  slider.style.transform = "translateX(-33%)";
+});
+prevBtn.addEventListener("click", () => {
+  slider.style.transform = "translateX(100%)";
+});
+console.log(slider);
